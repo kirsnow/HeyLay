6429ecd5ddacb817ef09fe20b6c633dbc69093cc
beak <tadanmf@gmail.com>
2017-01-19 18:11:43 +0900

    충돌 클래스 명 재수정

--- src/main/java/io/planb/statics/controller/StaticsController.java
+++ src/main/java/io/planb/statics/controller/StaticsController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.planb.statics.service.StaticsService;
 import io.planb.statics.vo.StaticsVO;
@@ -18,13 +19,14 @@ public class StaticsController {
 	private StaticsService service;
 	
 	/* 내가 많이 담은 사이트 */
+	@RequestMapping("/statics/savedSource.do")
 	public String selectSavedSource(HttpSession session,Model model) {
 		String member = (String) session.getAttribute("userVO");
 		
 		List<StaticsVO> statisticList = service.selectSavedSource(member);
 		model.addAttribute("statisticList", statisticList);
 		
-		return "";
+		return "admin/statistic";
 	}
 	
 }
