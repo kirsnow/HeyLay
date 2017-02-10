d61c3c160e2f551efd757c2816aa038596444ccb
beak <tadanmf@gmail.com>
2017-02-10 09:33:00 +0900

    git 충돌 오류 <<<<<< HEAD 코드 삭제

--- src/main/java/io/planb/search/controller/SearchController.java
+++ src/main/java/io/planb/search/controller/SearchController.java
@@ -6,20 +6,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-<<<<<<< HEAD
 import io.planb.contents.vo.ContentsListVO;
 import io.planb.contents.vo.ContentsVO;
-import io.planb.keywords.vo.KeywordsVO;
-=======
-import io.planb.contents.vo.ContentsVO;
->>>>>>> c97ae3d6693f3aab64094d5a3ec3b9e0e0142152
 import io.planb.member.vo.MemberVO;
 import io.planb.memo.service.MemoServiceImp;
 import io.planb.memo.vo.MemoVO;
