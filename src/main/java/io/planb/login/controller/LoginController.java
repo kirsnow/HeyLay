188601bf436cc729ab50726ff0733494241b9f89
Sungshinhye <tlsgp6760@gmail.com>
2017-02-14 10:36:44 +0900

    사라진 logout소환

--- src/main/java/io/planb/login/controller/LoginController.java
+++ src/main/java/io/planb/login/controller/LoginController.java
@@ -56,6 +56,13 @@ public class LoginController {
 		return "login/login";
 	}
 	
+    @RequestMapping("/login/logout.do")
+    public String logout(SessionStatus sessionStatus  /*HttpServletRequest request*/) {
+       
+    	sessionStatus.setComplete();
+        
+    	return "redirect:/";
+    }
 }
 
 
